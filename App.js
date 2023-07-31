bb7a9d9c1a75fe5854ccfb9cef524905c1fce110
Justin Shams <justinshams@gmail.com>
2023-07-31 15:05:25 -0700

    First commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Ionicons } from '@expo/vector-icons'
+
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MenuScreen from './screens/MenuScreen';
+import LoginScreen from './screens/LoginScreen';
+import FeedbackForm from './components/FeedbackForm';
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Drawer.Navigator initialRouteName="Login">
+            <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+            <Drawer.Screen name="Login" component={LoginScreen} />
+            <Drawer.Screen name="Menu" component={MenuScreen} />
+          </Drawer.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
